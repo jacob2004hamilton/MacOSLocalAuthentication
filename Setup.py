40b91e41bd37246c6d4e421d13404ab63653e7b5
jacob2004hamilton <92182228+jacob2004hamilton@users.noreply.github.com>
2023-02-01 10:01:20 +0000

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup
 
+from MacOSLocalAuthentication import __version__
+
+setup(
+    name='MacOSLocalAuthentication',
+    version=__version__,
+
+    url='https://github.com/jacob2004hamilton/MacOSLocalAuthentication',
+    author='Jacob Hamilton',
+    author_email='jacob@hamilton-home.com',
+
+    py_modules=['MacOSLocalAuthentication'],
+)
